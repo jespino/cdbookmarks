56dcf43cec2ff4754d6185fec1e7918bd587be6a
Jesús Espino <jesus.espino@kaleidos.net>
2012-05-02 16:24:45 +0200

    Initial version

---
+++ README.rst
@@ -0,0 +1,4 @@
+CDBookmarks
+-----------
+
+CDBoomkars is a simple app to easy bookmark directories on your filesystem.
---
+++ cdb.sh
@@ -0,0 +1,21 @@
+cdb()
+{
+    bookmark=`cdbookmarks --go $@`
+    if [ $? -eq 0 ]; then
+        counter=0
+        while read -r line;do
+            counter=$(($counter+1))
+            if [ $counter -eq 1 ]; then
+                $line
+            fi
+            if [ $counter -eq 2 ]; then
+                cd $line
+            fi
+            if [ $counter -eq 3 ]; then
+                $line
+            fi
+        done < <(cdbookmarks --go $@)
+	else
+		echo "Usage: $0 <bookmark-name>"
+	fi
+}
