9a178891e00eac585ec5a2dc254237cb9c7f5b4a
Jesús Espino <jesus.espino@kaleidos.net>
2012-05-11 21:15:44 +0200

    Now compatible with python3 and python2

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
-#from distutils.core import setup
 from setuptools import setup, find_packages
 
 setup(
     name = "cdbookmarks",
-    version = "0.0.1",
+    version = "0.0.2",
     description = "Simple directory bookmark tool",
     author = "Jesús Espino",
     author_email = "jespinog@gmail.com",
@@ -14,6 +13,9 @@ setup(
     license = "BSD",
     classifiers = [
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'License :: OSI Approved :: BSD License',
